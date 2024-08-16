25717722836d6e000b180e8286ff61583d120cb3
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-08-16 19:03:31 -0300

    update build system

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def read_file(path):
+def read_file(path: str):
     with open(path, 'r') as file:
         return file.read()
 
@@ -8,7 +8,6 @@ def build_index():
     system = [
         "system/widgets",
         "system/taskbar",
-        "system/start",
     ]
 
     apps = [
@@ -17,28 +16,32 @@ def build_index():
     ]
 
     ui = read_file("src/system/ui.html")
+    start = read_file("src/system/start.html")
+
+    menu_button = '<button id="@@APP_NAME" class="btn-some morp">@@APP_NAME</button>'
+    menu_items = ""
     windows = ""
 
     for path in apps:
         name = path.replace("apps/", "").capitalize()
         component = read_file(f"src/{path}.html")
         component = component.replace("@@APP_NAME", name)
-        windows = f"{windows}\n\n{component}"
+        item = menu_button.replace("@@APP_NAME", name)
+        menu_items = f"{menu_items}\n{item}\n"
+        windows = f"{windows}\n{component}\n"
 
+    start_menu = start.replace("@@MENU_ITEMS", menu_items)
 
     for path in system:
         component = read_file(f"src/{path}.html")
         name = path.replace("system/", "")
         ui = ui.replace(f"@@{name.upper()}", component)
-        # print(f"@@{name.upper()}", component)
 
+    ui = ui.replace("@@START", start_menu)
     index = ui.replace("@@APPS", windows)
 
     with open('src/index.html', 'w') as file:
         file.write(index)
         file.close()
-
-    # print(index)
-
 
 build_index()
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,3 +24,31 @@ $(document).ready(function () {
   updateClock(); // Initial call to display time immediately
   setInterval(updateClock, 1000); // Update time every second
 });
+
+// Taskbar wallpaper switch
+$(document).ready(function () {
+  function sys_switch_desktop(number) {
+    const images = [
+      "url(wallpapers/wp1.jpg)",
+      "url(wallpapers/wp2.jpg)",
+      "url(wallpapers/wp3.jpg)",
+      "url(wallpapers/wp4.jpg)",
+      "url(wallpapers/wp5.jpg)",
+    ];
+
+    if (number >= 1 && number <= images.length) {
+      $("body").css("background-image", images[number - 1]);
+    } else {
+      console.log(
+        "Invalid number. Please provide a number between 1 and " +
+          images.length,
+      );
+    }
+  }
+
+  // Attach click event to taskbar buttons
+  $(".taskbar-btn").on("click", function () {
+    const number = $(this).data("number");
+    sys_switch_desktop(number);
+  });
+});
