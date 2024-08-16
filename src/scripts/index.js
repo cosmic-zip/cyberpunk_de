aa6cbe46b7935916b57e5e4ede2abdda87f1fdcc
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-08-16 17:13:00 -0300

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,24 @@ $(document).ready(function () {
     $("#sidebar").toggle();
   });
 });
+
+$(document).ready(function () {
+  function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    // Add leading zeros  if necessary
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    var timeString = hours + ":" + minutes + ":" + seconds;
+
+    $("#clock").text(timeString);
+  }
+
+  updateClock(); // Initial call to display time immediately
+  setInterval(updateClock, 1000); // Update time every second
+});
