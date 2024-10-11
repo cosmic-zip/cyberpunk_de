fdb8e3e0f14bb9d3040f0f2c9fbdddd1873b5c13
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-10-11 08:43:56 -0300

    Update tauri version from v1 to v2

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install libwebkit2gtk-4.1-dev \
+  build-essential \
+  curl \
+  wget \
+  file \
+  libxdo-dev \
+  libssl-dev \
+  libayatana-appindicator3-dev \
+  librsvg2-dev \
+  libsoup2.4 -y
+sudo apt-get install javascriptcoregtk-4.1 libsoup-3.0 webkit2gtk-4.1 -y
